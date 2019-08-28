db4886b6087cdcdb1b2b2a1198140daa8d39480b
trickyBytes <richard.tiffin@kaluza.com>
2019-08-28 16:43:07 +0100

    chapter-8: Build a burger from state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Burger.module.scss';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from "./Burger.module.scss";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = (props) => {
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
-            <BurgerIngredient type="bread-bottom" />
-        </div>
-    );
+const burger = props => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 
 export default burger;
