12a585c0177a189a5e4588bd28de8b1d81acce61
trickyBytes <richard.tiffin@kaluza.com>
2019-08-29 09:13:55 +0100

    chapter-8: Display msg if no ingredients in state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,21 @@ import classes from "./Burger.module.scss";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = props => {
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    //Flatten array
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0){
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }
+
+  console.log(transformedIngredients);
 
   return (
     <div className={classes.Burger}>
