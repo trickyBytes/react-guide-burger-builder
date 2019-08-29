77392e520440bd8b9a6e23c9491c00bef528fef8
trickyBytes <richard.tiffin@kaluza.com>
2019-08-29 10:54:14 +0100

    chapter-8: Make add burdger bits control work

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import classes from './BuildControls.module.scss'
-import BuildControl from './BuildControl/BuildControl'
+import classes from "./BuildControls.module.scss";
+import BuildControl from "./BuildControl/BuildControl";
 
 const controls = [
-    { lable: 'Salad',  type: 'salad'},
-    { lable: 'Bacon',  type: 'bacon'},
-    { lable: 'Cheese',  type: 'cheese'},
-    { lable: 'Meat',  type: 'meat'},
-]
+  { lable: "Salad", type: "salad" },
+  { lable: "Bacon", type: "bacon" },
+  { lable: "Cheese", type: "cheese" },
+  { lable: "Meat", type: "meat" }
+];
 
-const buildControls = (props) => (
-    <div className={classes.BuildControls}>
-        {controls.map(ctrl => (
-            <BuildControl key={ctrl.lable} label={ctrl.lable} />
-        ))}
-    </div>
-)
+const buildControls = props => (
+  <div className={classes.BuildControls}>
+    {controls.map((ctrl) => (
+      <BuildControl
+        key={ctrl.lable}
+        label={ctrl.lable}
+        added={() => props.ingredientAdded(ctrl.type)}
+      />
+    ))}
+  </div>
+);
 
 export default buildControls;
