409586e626687879a4c4e951a7d6d0a23890ca8f
trickyBytes <richard.tiffin@kaluza.com>
2019-08-30 10:21:04 +0100

    chapter-7: Added price to order summary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -101,6 +101,7 @@ class BurgerBuilder extends Component {
         >
           <OrderSummary
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
             purchaseCancelled={this.purchaseCancelHander}
             purchaseContinued={this.purchaseContinueHandler}
           />
