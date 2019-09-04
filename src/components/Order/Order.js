a03afd4e799fab9d984e379e1a83d27873d7c443
trickyBytes <richard.tiffin@kaluza.com>
2019-09-04 09:21:19 +0100

    chapter-12:  Add and link to order page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import classes from "./Order.module.scss";
+
+const order = props => (
+    <div className={classes.Order}>
+        <p>Ingredients: Salad (1)</p>
+        <p>Price: <strong>GBP 5.45</strong></p>
+    </div>
+);
+
+export default order;
